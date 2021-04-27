6b519e55acac981c97519c8970d0d36fec4b18f9
Nablinghc <71384026+Nablinghc@users.noreply.github.com>
2021-04-27 12:25:42 +0200

    2323
    2323

--- src/components/Front.js
+++ src/components/Front.js
@@ -12,7 +12,7 @@ const Front = () => {
                 <img src="/assets/img/front-what-we-do.png" alt="What we do banner"></img>
                 <img src="/assets/img/front-maintainable.png" alt="What we do banner"></img>
                 <img src="/assets/img/front-get-in-touch.png" alt="What we do banner"></img>
-                <h6>And yet... more to come</h6>
+                <h6 id="AYMTM">And yet... more to come</h6>
             </section>
         </Fragment>
     )
